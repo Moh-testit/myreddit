78d9e15f03bafb908d5b81f2fa6f9316fce4599e
Mohamed MAZU <mazu@pop-os.localdomain>
2021-10-23 18:31:19 +0100

    Created a new Expo app

--- App.js
+++ App.js
@@ -1,12 +1,21 @@
-// App.js
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 
-import React from 'react'
-import Search from './components/Search'
-
-export default class App extends React.Component {
-  render() {
-    return (
-      <Search style={{backgroundColor: 'green'}}/>
-    )
-  }
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
